7690cea8d0ac0ece7747ab582bafd0dbc2eda4cd
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-15 13:09:52 +0800

    Initial commit

---
+++ 密碼.js
@@ -0,0 +1,10 @@
+function send(rep) {
+    let ans = "worldwartwo";
+    if(ans == rep) {
+        document.write('<a href = "https://gather.town/app/9CXQnA0r5RBpqFyI/final%20act3"> <img src = "./portal.png" loading = "eager"></a>');
+        alert("好像有了甚麼動靜\n下面那是傳送門嗎？");
+    }
+    else {
+        alert("密碼並不正確，再想想吧");
+    }
+}
