bae684ce74b309e86d9e5a1bcb7928d11f19dd98
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-15 16:21:08 +0800

    Update 密碼.js

--- 密碼.js
+++ 密碼.js
@@ -1,7 +1,7 @@
 function send(rep) {
     let ans = "BreakALLsodianorz";
     if(ans == rep) {
-        document.write('<p>https://gather.town/invite?token=kj05UmXq</p><p>請自行複製(我就爛)</p>');
+        document.write('<p>https://gather.town/app/9CXQnA0r5RBpqFyI/final%20act3</p><p>請自行複製(我就爛)</p>');
         alert("好像有了甚麼動靜\n下面那是傳送門嗎？");
     }
     else {
