2fa6888866b08959a6425058b718ce6a5c25ba24
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-16 22:16:50 +0800

    Add files via upload

---
+++ router.js
@@ -0,0 +1,32 @@
+function route(group) {
+	switch(group) {
+		case 1: 
+			return "https://gather.town/invite?token=mzcydF1N";
+		case 2:
+			return "https://gather.town/invite?token=Joypvhw7";
+		case 3:
+			return "https://gather.town/invite?token=4iUmN3Rr";
+		case 4:
+			return "https://gather.town/invite?token=rmtAWaaQ";
+		case 5:
+			return "https://gather.town/invite?token=MPbbX7si";
+		case 6:
+			return "https://gather.town/invite?token=PLDXea2G";
+		case 7:
+			return "https://gather.town/invite?token=sSfZV8yj";
+		case 8:
+			return "https://gather.town/invite?token=EdY1ZOp5";
+		case 9:
+			return "https://gather.town/invite?token=GSwxDznX";
+		case 10:
+			return "https://gather.town/invite?token=eFkjvNpF";
+		case 11:
+			return "https://gather.town/invite?token=CsQdfByV";
+		case 12:
+			return "https://gather.town/invite?token=LU0aPUsS";
+		case 13:
+			return "https://gather.town/invite?token=yDj4LhVi";
+		case 14:
+			return "https://gather.town/invite?token=u4BcV4hz";
+	}
+}
