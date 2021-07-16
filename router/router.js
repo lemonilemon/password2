11ad20f02dd428dfdf927d736d9e246cb004b630
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-17 01:17:54 +0800

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,46 +1,46 @@
 function route(group) {
 	switch(group) {
 		case 1: 
-			return "https://gather.town/invite?token=mzcydF1N";
+			return "https://gather.town/app/RbyydKkz1brpseu8/act3-1";
 			break;
 		case 2:
-			return "https://gather.town/invite?token=Joypvhw7";
+			return "https://gather.town/app/EckRIrzICamKK5oA/act3-2";
 			break;
 		case 3:
-			return "https://gather.town/invite?token=4iUmN3Rr";
+			return "https://gather.town/app/kYUGfyXjkmyqF5HA/act3-3";
 			break;
 		case 4:
-			return "https://gather.town/invite?token=rmtAWaaQ";
+			return "https://gather.town/app/fQOLlA8nRWjuMGnj/act3-4";
 			break;
 		case 5:
-			return "https://gather.town/invite?token=MPbbX7si";
+			return "https://gather.town/app/XWS4NM7OYscUHZQ8/act3-5";
 			break;
 		case 6:
-			return "https://gather.town/invite?token=PLDXea2G";
+			return "https://gather.town/app/ytjkoeRqMWadOOwn/act3-6";
 			break;
 		case 7:
-			return "https://gather.town/invite?token=sSfZV8yj";
+			return "https://gather.town/app/iiEg7miMGdlWdSoJ/act3-7";
 			break;
 		case 8:
-			return "https://gather.town/invite?token=EdY1ZOp5";
+			return "https://gather.town/app/zt4Dfy67Z3rBuNXi/act3-8";
 			break;
 		case 9:
-			return "https://gather.town/invite?token=GSwxDznX";
+			return "https://gather.town/app/XzQofbdBB0MlkOvE/act3-9";
 			break;
 		case 10:
-			return "https://gather.town/invite?token=eFkjvNpF";
+			return "https://gather.town/app/Ge4cP6XlPcoW7dFZ/act3-10";
 			break;
 		case 11:
-			return "https://gather.town/invite?token=CsQdfByV";
+			return "https://gather.town/app/eLMDdqQ1DRfohVhY/act3-11";
 			break;
 		case 12:
-			return "https://gather.town/invite?token=LU0aPUsS";
+			return "https://gather.town/app/ncx8xx3ysY7n7ars/act3-12";
 			break;
 		case 13:
-			return "https://gather.town/invite?token=yDj4LhVi";
+			return "https://gather.town/app/q21Uw1MGj91Yjsml/act3-13";
 			break;
 		case 14:
-			return "https://gather.town/invite?token=u4BcV4hz";
+			return "https://gather.town/app/zIovRa2fQ73WLi86/act3-14";
 			break;
 		default:
 			return "請輸入有效數據";
