da3c0ae891492fb89dd2577966365c84fe789e0f
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-16 22:28:23 +0800

    Update 密碼.js

--- 密碼.js
+++ 密碼.js
@@ -1,8 +1,8 @@
 function send(rep) {
     let ans = "BreakALLsodianorz";
     if(ans == rep) {
-        document.write('<p>https://gather.town/app/9CXQnA0r5RBpqFyI/final%20act3</p><p>請自行複製(我就爛)</p>');
         alert("好像有了甚麼動靜\n下面那是傳送門嗎？");
+        document.write("<a href = "./router/router.html"><div>我是傳送門</div></a>");
     }
     else {
         alert("密碼並不正確，再想想吧");
